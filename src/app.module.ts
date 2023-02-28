c3d4f3bbb0b2f08016ca9fd6e0469479c69684e7
Hasan Demir <hsndmr5044@hotmail.com>
2023-03-01 00:04:25 +0300

    Add app module

---
+++ src/app.module.ts
@@ -0,0 +1,50 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { AcceptLanguageResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
+import { LoggerModule } from './logger/logger.module';
+import * as path from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from './jwt/jwt.module';
+import { CryptoModule } from './crypto/crypto.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+      ],
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    LoggerModule,
+    CryptoModule,
+    JwtModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { AppLogger } from './logger/app-logger.service';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const documentBuilderConfig = new DocumentBuilder()
+    .setTitle('')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('auth')
+    .addBearerAuth()
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(
+    app,
+    documentBuilderConfig,
+  );
+  SwaggerModule.setup('api', app, swaggerDocument);
+
+  app.useLogger(app.get(AppLogger));
+
+  await app.listen(3000);
+}
+bootstrap();
