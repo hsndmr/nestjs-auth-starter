16490aba9c803d89cda17b289343ea2f7368fced
Hasan Demir <hsndmr5044@hotmail.com>
2023-03-02 00:04:04 +0300

    Add act name for test cases

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -28,91 +28,102 @@ describe('UserService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should create a user', async () => {
-    const password = userFactory.faker.internet.password();
+  describe('createUser', () => {
+    it('should create a user', async () => {
+      const password = userFactory.faker.internet.password();
 
-    const user = await service.create({
-      email: userFactory.faker.internet.email(),
-      name: userFactory.faker.name.firstName(),
-      lastName: userFactory.faker.name.lastName(),
-      password: password,
+      const user = await service.create({
+        email: userFactory.faker.internet.email(),
+        name: userFactory.faker.name.firstName(),
+        lastName: userFactory.faker.name.lastName(),
+        password: password,
+      });
+
+      expect(user).toBeDefined();
+      expect(user.password).not.toEqual(password);
     });
-
-    expect(user).toBeDefined();
-    expect(user.password).not.toEqual(password);
   });
 
-  it('should create a user from dto', async () => {
-    const user = await service.createFromDto({
-      email: userFactory.faker.internet.email(),
-      name: userFactory.faker.name.firstName(),
-      lastName: userFactory.faker.name.lastName(),
-      password: userFactory.faker.internet.password(),
-    });
+  describe('createFromDto', () => {
+    it('should create a user from dto', async () => {
+      const user = await service.createFromDto({
+        email: userFactory.faker.internet.email(),
+        name: userFactory.faker.name.firstName(),
+        lastName: userFactory.faker.name.lastName(),
+        password: userFactory.faker.internet.password(),
+      });
 
-    expect(user).toBeDefined();
+      expect(user).toBeDefined();
+    });
+  });
+  describe('findOneByEmail', () => {
+    it('should find a user by email', async () => {
+      const user = await userFactory.create();
+
+      const foundUser = await service.findOneByEmail(user.email);
+
+      expect(foundUser).toBeDefined();
+    });
   });
 
-  it('should find a user by email', async () => {
-    const user = await userFactory.create();
+  describe('findOneById', () => {
+    it('should find a user by id', async () => {
+      const user = await userFactory.create();
 
-    const foundUser = await service.findOneByEmail(user.email);
+      const foundUser = await service.findOneById(user.id);
 
-    expect(foundUser).toBeDefined();
+      expect(foundUser).toBeDefined();
+    });
   });
 
-  it('should find a user by id', async () => {
-    const user = await userFactory.create();
+  describe('createToken', () => {
+    it('should create a token', async () => {
+      const user = await userFactory.create();
 
-    const foundUser = await service.findOneById(user.id);
+      const token = await service.createToken({
+        user: user,
+      });
 
-    expect(foundUser).toBeDefined();
+      expect(token).toBeDefined();
+    });
+
+    it('should create a token with scopes', async () => {
+      const user = await userFactory.create();
+
+      await service.createToken({
+        user: user,
+        scopes: ['test'],
+      });
+
+      expect(user.tokens[0].scopes).toEqual(['test']);
+    });
   });
 
-  it('should create a token', async () => {
-    const user = await userFactory.create();
+  describe('findUserByJtiAndId', () => {
+    it('should find a token by jti and user id', async () => {
+      // Arrange
+      const user = await userFactory.create();
 
-    const token = await service.createToken({
-      user: user,
+      await service.createToken({
+        user: user,
+      });
+
+      const token = await service.createToken({
+        user: user,
+      });
+
+      const verifiedToken = await jwtService.verify(token);
+
+      const { jti, sub } = verifiedToken;
+
+      // Act
+      const foundToken = await service.findUserByJtiAndId(jti, sub);
+
+      // Assert
+      expect(foundToken._id).toEqual(user._id);
+      expect(foundToken.tokens.length).toEqual(1);
+      expect(foundToken.tokens[0].jti).toEqual(user.tokens[1].jti);
     });
-
-    expect(token).toBeDefined();
-  });
-
-  it('should create a token with scopes', async () => {
-    const user = await userFactory.create();
-
-    await service.createToken({
-      user: user,
-      scopes: ['test'],
-    });
-
-    expect(user.tokens[0].scopes).toEqual(['test']);
-  });
-
-  it('should find a token by jti and user id', async () => {
-    // Arrange
-    const user = await userFactory.create();
-
-    await service.createToken({
-      user: user,
-    });
-
-    const token = await service.createToken({
-      user: user,
-    });
-
-    const verifiedToken = await jwtService.verify(token);
-
-    const { jti, sub } = verifiedToken;
-
-    // Act
-    const foundToken = await service.findUserByJtiAndId(jti, sub);
-
-    // Assert
-    expect(foundToken._id).toEqual(user._id);
-    expect(foundToken.tokens.length).toEqual(1);
-    expect(foundToken.tokens[0].jti).toEqual(user.tokens[1].jti);
   });
 
   describe('tokenCan', () => {
