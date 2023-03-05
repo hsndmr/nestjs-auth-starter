af51460bfdc160b2961d3e98218872aa66c19030
Hasan Demir <hsndmr5044@hotmail.com>
2023-03-05 05:24:58 +0300

    Add ApiProperty

--- src/auth/dtos/create-user.dto.ts
+++ src/auth/dtos/create-user.dto.ts
@@ -1,15 +1,32 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@test.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'User name',
+    example: 'name',
+  })
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'User last name',
+    example: 'last name',
+  })
   @IsNotEmpty()
   lastName: string;
 
+  @ApiProperty({
+    description: 'User password',
+    example: 'password',
+  })
   @IsNotEmpty()
   password: string;
 }
--- src/auth/dtos/login-user.dto.ts
+++ src/auth/dtos/login-user.dto.ts
@@ -1,9 +1,18 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginUserDto {
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@test.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'User password',
+    example: 'password',
+  })
   @IsNotEmpty()
   password: string;
 }
