360964fe4c3ef9c055a8a273f92e369967de76a5
Hasan Demir <hsndmr5044@hotmail.com>
2023-03-08 12:55:16 +0300

    Add `SWAGGER_ENABLED` and `PORT` variables

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AppLogger } from './logger/app-logger.service';
 import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
+import { setupSwagger } from './swagger/setup';
+import { stringToBool } from './utils/common/string-to-bool';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
 
+  const configService = app.get<ConfigService>(ConfigService);
+
   app.useGlobalPipes(new ValidationPipe());
 
-  const documentBuilderConfig = new DocumentBuilder()
-    .setTitle('')
-    .setDescription('')
-    .setVersion('1.0')
-    .addTag('auth')
-    .addBearerAuth()
-    .build();
-  const swaggerDocument = SwaggerModule.createDocument(
-    app,
-    documentBuilderConfig,
-  );
-  SwaggerModule.setup('api', app, swaggerDocument);
+  if (stringToBool(configService.get('SWAGGER_ENABLED'))) {
+    setupSwagger(app);
+  }
 
   app.useLogger(app.get(AppLogger));
   app.use(cookieParser());
 
-  await app.listen(3000);
+  const port = configService.get<number>('PORT');
+  await app.listen(port ?? 3000);
 }
 bootstrap();
