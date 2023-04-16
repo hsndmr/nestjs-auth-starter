aa4bd5d1bad548e1ed5746c7f6d6348f0acbc46f
Hasan Demir <hsndmr5044@hotmail.com>
2023-04-16 23:17:25 +0300

    feat(utils): add error handling to startMachine function

--- src/utils/machine/start-machine.ts
+++ src/utils/machine/start-machine.ts
@@ -1,12 +1,19 @@
 import { interpret, State } from 'xstate';
+import { HttpException } from '@nestjs/common';
 
 export const startMachine = <TContext>(
   service: ReturnType<typeof interpret<any>>,
 ): Promise<State<TContext>> => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     service
       .onDone(() => {
-        resolve(service.getSnapshot());
+        const snapshot = service.getSnapshot();
+        const { errorCode, errorMessage } = snapshot.context;
+        if (errorCode) {
+          reject(new HttpException(errorMessage, errorCode));
+          return;
+        }
+        resolve(snapshot);
       })
       .start();
   });
